e6e0b3b42710edd5ee99c956fcdef74e9c2c370b
Vasili Perahud <vasili3d@users.noreply.github.com>
2017-03-16 15:47:43 +0300

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,39 @@
+//Singleton
 #include <iostream>
 
-int main()
+using namespace std;
+#define singleton X::Instance()
+
+class X {
+private: //does not access
+	X() = default; //constructor
+	X(X&) = default; //copy constructor
+	X& operator=(const X&) = default; //assigner
+
+public:  //does access
+	static X* Instance(); //static
+	
+	int hi();
+	void xx() { cout << "xx" << endl; }
+};
+
+X* X::Instance()
 {
+	static X instance; //static
+	return &instance;
+}
 
+int X::hi() {
+	int a = 1;
+	return a;
+}
 
+int main() {
+	// X a; // Error!
+	cout << X::Instance()->hi() << endl;
+	singleton->xx();
+
+	cin.get();
 	return 0;
+
 }
